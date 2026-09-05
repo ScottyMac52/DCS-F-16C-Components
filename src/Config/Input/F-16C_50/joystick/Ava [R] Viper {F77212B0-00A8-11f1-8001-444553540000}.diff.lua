8fea730f202733fa0e693520993e57c4a60dc251
Scott McIntosh <scottymac40love@gmail.com>
2026-09-05 16:52:08 -0700

    re-scaffolded the F-16 and removed the Warthog Throttle

--- src/Config/Input/F-16C_50/joystick/Ava [R] Viper {F77212B0-00A8-11f1-8001-444553540000}.diff.lua
+++ src/Config/Input/F-16C_50/joystick/Ava [R] Viper {F77212B0-00A8-11f1-8001-444553540000}.diff.lua
@@ -122,30 +122,6 @@ local diff = {
 			},
 			["name"] = "Countermeasures Management Switch - Right",
 		},
-		["d3018pnilu3018cd16vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN19",
-				},
-			},
-			["name"] = "Expand/FOV Button - Depress",
-		},
-		["d3020pnilu3020cd16vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN1",
-				},
-			},
-			["name"] = "CAMERA/GUN Trigger - FIRST DETENT",
-		},
-		["d3021pnilu3021cd16vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN6",
-				},
-			},
-			["name"] = "CAMERA/GUN Trigger - SECOND DETENT",
-		},
 		["d3022pnilu3022cd16vd1vpnilvu0"] = {
 			["added"] = {
 				[1] = {
